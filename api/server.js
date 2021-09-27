96d8c3f13594afa53c400a67bcaab72fd012eb2a
Spenser Wellen <spenserwellen@gmail.com>
2021-09-26 22:16:33 -0500

    backend of route is working well for connection to external API!

--- api/server.js
+++ api/server.js
@@ -1,136 +1,162 @@
 import express from 'express';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from "./models/User.js";
-import Comment from "./models/Comment.js";
-import VotingRoutes from "./VotingRoutes.js";
+import User from './models/User.js';
+import Comment from './models/Comment.js';
+import VotingRoutes from './VotingRoutes.js';
+import request from 'request';
 
 const secret = 'secret123';
 const app = express();
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+	})
+);
 
 app.use(VotingRoutes);
 
 function getUserFromToken(token) {
-  const userInfo = jwt.verify(token, secret);
-  return User.findById(userInfo.id);
+	const userInfo = jwt.verify(token, secret);
+	return User.findById(userInfo.id);
 }
 
-await mongoose.connect('mongodb://localhost:27017/reddit', {useNewUrlParser:true,useUnifiedTopology:true,});
+await mongoose.connect('mongodb://localhost:27017/reddit', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on('error', console.log);
 
 app.get('/', (req, res) => {
-  res.send('ok');
+	res.send('ok');
 });
 
 app.post('/register', (req, res) => {
-  const {email,username} = req.body;
-  const password = bcrypt.hashSync(req.body.password, 10);
-  const user = new User({email,username,password});
-  user.save().then(user => {
-    jwt.sign({id:user._id}, secret, (err, token) => {
-      if (err) {
-        console.log(err);
-        res.sendStatus(500);
-      } else {
-        res.status(201).cookie('token', token).send();
-      }
-    });
-  }).catch(e => {
-    console.log(e);
-    res.sendStatus(500);
-  });
+	const { email, username } = req.body;
+	const password = bcrypt.hashSync(req.body.password, 10);
+	const user = new User({ email, username, password });
+	user
+		.save()
+		.then((user) => {
+			jwt.sign({ id: user._id }, secret, (err, token) => {
+				if (err) {
+					console.log(err);
+					res.sendStatus(500);
+				} else {
+					res.status(201).cookie('token', token).send();
+				}
+			});
+		})
+		.catch((e) => {
+			console.log(e);
+			res.sendStatus(500);
+		});
 });
 
 app.get('/user', (req, res) => {
-  const token = req.cookies.token;
+	const token = req.cookies.token;
 
-  getUserFromToken(token)
-    .then(user => {
-      res.json({username:user.username});
-    })
-    .catch(err => {
-      console.log(err);
-      res.sendStatus(500);
-    });
-
+	getUserFromToken(token)
+		.then((user) => {
+			res.json({ username: user.username });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(500);
+		});
 });
 
 app.post('/login', (req, res) => {
-  const {username, password} = req.body;
-  User.findOne({username}).then(user => {
-    if (user && user.username) {
-      const passOk = bcrypt.compareSync(password, user.password);
-      if (passOk) {
-        jwt.sign({id:user._id}, secret, (err, token) => {
-          res.cookie('token', token).send();
-        });
-      } else {
-        res.status(422).json('Invalid username or password');
-      }
-    } else {
-      res.status(422).json('Invalid username or password');
-    }
-  });
+	const { username, password } = req.body;
+	User.findOne({ username }).then((user) => {
+		if (user && user.username) {
+			const passOk = bcrypt.compareSync(password, user.password);
+			if (passOk) {
+				jwt.sign({ id: user._id }, secret, (err, token) => {
+					res.cookie('token', token).send();
+				});
+			} else {
+				res.status(422).json('Invalid username or password');
+			}
+		} else {
+			res.status(422).json('Invalid username or password');
+		}
+	});
 });
 
 app.post('/logout', (req, res) => {
-  res.cookie('token', '').send();
+	res.cookie('token', '').send();
 });
 
 app.get('/comments', (req, res) => {
-  Comment.find({rootId:null}).sort({postedAt: -1}).then(comments => {
-    res.json(comments);
-  });
+	Comment.find({ rootId: null })
+		.sort({ postedAt: -1 })
+		.then((comments) => {
+			res.json(comments);
+		});
 });
 
 app.get('/comments/root/:rootId', (req, res) => {
-  Comment.find({rootId:req.params.rootId}).sort({postedAt: -1}).then(comments => {
-    res.json(comments);
-  });
+	Comment.find({ rootId: req.params.rootId })
+		.sort({ postedAt: -1 })
+		.then((comments) => {
+			res.json(comments);
+		});
 });
 
 app.get('/comments/:id', (req, res) => {
-  Comment.findById(req.params.id).then(comment => {
-    res.json(comment);
-  });
+	Comment.findById(req.params.id).then((comment) => {
+		res.json(comment);
+	});
 });
 
 app.post('/comments', (req, res) => {
-  const token = req.cookies.token;
-  if (!token) {
-    res.sendStatus(401);
-    return;
-  }
-  getUserFromToken(token)
-    .then(userInfo => {
-      const {title,body,parentId,rootId} = req.body;
-      const comment = new Comment({
-        title,
-        body,
-        author:userInfo.username,
-        postedAt:new Date(),
-        parentId,
-        rootId,
-      });
-      comment.save().then(savedComment => {
-        res.json(savedComment);
-      }).catch(console.log);
-    })
-    .catch(() => {
-      res.sendStatus(401);
-    });
+	const token = req.cookies.token;
+	if (!token) {
+		res.sendStatus(401);
+		return;
+	}
+	getUserFromToken(token)
+		.then((userInfo) => {
+			const { title, body, parentId, rootId } = req.body;
+			const comment = new Comment({
+				title,
+				body,
+				author: userInfo.username,
+				postedAt: new Date(),
+				parentId,
+				rootId,
+			});
+			comment
+				.save()
+				.then((savedComment) => {
+					res.json(savedComment);
+				})
+				.catch(console.log);
+		})
+		.catch(() => {
+			res.sendStatus(401);
+		});
+});
+
+app.get('/news', function (req, res) {
+	request(
+		'https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty',
+		function (error, response, body) {
+			const obj = JSON.parse(body);
+			// console.log(obj);
+			res.json(obj);
+		}
+	);
 });
 
 app.listen(4000);
