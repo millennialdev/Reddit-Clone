638048e84520236abc324fa3ff9b5827078059a7
Spenser Wellen <spenserwellen@gmail.com>
2021-09-26 19:22:43 -0500

    built out some of the overall design a bit more

--- client/src/BoardHeader.js
+++ client/src/BoardHeader.js
@@ -1,21 +1,34 @@
 function BoardHeader() {
-  return (
-    <>
-      <div className="h-20 bg-cover" style={{backgroundImage:'url("https://styles.redditmedia.com/t5_2qs0q/styles/bannerBackgroundImage_7glcgg5ymxp21.png?width=4000&s=9684bc66e812b8730ad694c3f454da8c00a493d7")'}}>
-      </div>
-      <div className="bg-reddit_dark-brighter">
-        <div className="mx-6 relative flex">
-          <div className="h-20 w-20 rounded-full overflow-hidden relative -top-3 border-4 border-white bg-white">
-            <img src="https://styles.redditmedia.com/t5_2qs0q/styles/communityIcon_5ey8lzmwmxp21.png?width=256&s=5a85d5c682f40e3cf317c560b219585ac0afce78" alt=""/>
-          </div>
-          <div className="pt-2 pl-4">
-            <h1 className="text-gray-300 text-3xl">webdev: reddit for web developers</h1>
-            <h5 className="text-gray-500">r/webdev</h5>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className='h-40 bg-cover'
+				style={{
+					backgroundImage:
+						'url("https://preview.redd.it/xw6wqhhjubh31.jpg?width=2400&format=pjpg&auto=webp&s=32690f33b69e599ed11ea3e7c0e6286c0770245e")',
+					backgroundPosition: 'center center',
+				}}></div>
+			<div className='bg-reddit_dark-brighter'>
+				<div className='mx-6 relative flex'>
+					<div className='h-20 w-20 rounded-full overflow-hidden relative -top-3 border-4 border-white bg-white'>
+						<img
+							src='https://avatars.githubusercontent.com/u/6439723?v=4'
+							alt=''
+						/>
+					</div>
+					<div className='pt-2 pl-4'>
+						<h1 className='text-gray-300 text-3xl'>
+							Spenser Zone: My own personal subreddit
+						</h1>
+						<p className='text-gray-300 text-1xl mt-1'>
+							Hi, My Name is Spenser: This is my clone of Reddit
+						</p>
+						<h5 className='text-gray-500'>r/spenser-zone</h5>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export  default BoardHeader;
+export default BoardHeader;
